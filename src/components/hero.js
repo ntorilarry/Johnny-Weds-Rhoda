cb415537b5c5559fef90481694cb7f42d1f777cd
unknown <ntorilarry@gmail.com>
2022-08-09 11:29:59 +0000

    dev-larry

--- src/components/hero.js
+++ src/components/hero.js
@@ -16,9 +16,7 @@ function Hero() {
           <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
               <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
-                <a href="/" className="mb-6 sm:mx-auto">
-                  <div className="flex items-center justify-center w-12 h-12 rounded-full bg-teal-accent-400"></div>
-                </a>
+                
                 <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                   <h2 className="max-w-lg mb-6 font-sans text-5xl text-center leading-none tracking-tight text-white sm:text-7xl md:mx-auto h-header">
                     Johnny & Rhoda
