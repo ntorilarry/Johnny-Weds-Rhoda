1e7c09fd6fb17491ed64f2003069835bedca3874
unknown <ntorilarry@gmail.com>
2022-08-16 16:35:44 +0000

    dev-larry

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -14,51 +14,79 @@ import Navbar from "./navbar";
 function Gallery() {
   return (
     <div className="bg-[#713C25]">
-      <Navbar/>
-    <div className="max-w-6xl mx-auto">
-      <div className="grid max-w-4xl mx-auto p-8">
-        <h1 className="text-3xl p-6 text-white mx-auto">Gallery</h1>
-        <img
-          className="w-full block rounded-b"
-          src={Gal1}
-          alt="A photo of a fox"
-        />
-        <div className="text-3xl p-6"></div>
-        <img
-          className="w-full block rounded-b"
-          src={Gal2}
-          alt="A photo of a woman"
-        />
+      <Navbar />
+      <div className="max-w-6xl mx-auto">
+        <div className="grid max-w-4xl mx-auto p-8">
+          <h1 className="text-3xl p-6 text-white mx-auto">Gallery</h1>
+          <img
+            className="w-full block rounded-b"
+            src={Gal1}
+            alt="A photo of a fox"
+          />
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal2}
+            alt="A photo of a woman"
+          />
 
-        <div className="text-3xl p-6"></div>
-        <img
-          className="w-full block rounded-b"
-          src={Gal3}
-          alt="A photo of some reeds"
-        />
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal3}
+            alt="A photo of some reeds"
+          />
 
-        <div className="text-3xl p-6"></div>
-        <img
-          className="w-full block rounded-b"
-          src={Gal4}
-          alt="A photo of a man"
-        />
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal4}
+            alt="A photo of a man"
+          />
 
-        <div className="text-3xl p-6"></div>
-        <img
-          className="w-full block rounded-b"
-          src={Gal5}
-          alt="A photo of a forest"
-        />
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal5}
+            alt="A photo of a forest"
+          />
 
-        <div className="text-3xl p-6"></div>
-        <img
-          className="w-full block rounded-b"
-          src={Gal6}
-          alt="A photo of a bird"
-        />
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal6}
+            alt="A photo of a bird"
+          />
+
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal7}
+            alt="A photo of a bird"
+          />
+
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal8}
+            alt="A photo of a bird"
+          />
+
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal9}
+            alt="A photo of a bird"
+          />
+
+          <div className="text-3xl p-6"></div>
+          <img
+            className="w-full block rounded-b"
+            src={Gal10}
+            alt="A photo of a bird"
+          />
+        </div>
       </div>
-    </div>
     </div>
   );
 }
